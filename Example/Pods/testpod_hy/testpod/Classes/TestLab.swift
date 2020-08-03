b1213bf3a5c2d46d3df6fc600a71702329aa2909
yf <yf@Han-MacBook-Pro-1152.local>
2020-08-03 16:49:34 +0800

    更新

--- Example/Pods/testpod_hy/testpod/Classes/TestLab.swift
+++ Example/Pods/testpod_hy/testpod/Classes/TestLab.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public class LabUtil {
    public class func printtest(_ name:String) -> String {
-        NSLog("name = %@", name)
+        NSLog("0.1.1 name = %@", name)
         return "name"
     }
 }
